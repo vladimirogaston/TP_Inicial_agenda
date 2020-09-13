6c879dadcd9efddfc4d3b5e314d7a2d6ce7f6b4c
Vladimiro gaston <vladimirogaston@gmail.com>
2020-09-13 16:04:01 -0300

    add to scriptAgenda.sql tables localidades & tiposcontacto & updt table personas

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -1,9 +1,35 @@
 CREATE DATABASE `grupo_11`;
 USE grupo_11;
-CREATE TABLE `personas`
-(
-  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
+
+DROP TABLE IF EXISTS `Personas`;
+CREATE TABLE `Personas`(
+PersonaID INT NOT NULL AUTO_INCREMENT,
+Nombre VARCHAR(255),
+Apellido VARCHAR(255),
+Telefono INT,
+Email VARCHAR(255),
+FechaCumpleaños DATE,
+TipoContactoID INT,
+Calle VARCHAR(255),
+Altura INT,
+Piso INT,
+Departamento VARCHAR(3),
+LocalidadID INT,
+PRIMARY KEY (PersonaID),
+FOREIGN KEY (TipoContactoID) REFERENCES TiposContacto (TipoContactoID),
+FOREIGN KEY (LocalidadID) REFERENCES Localidades (LocalidadID)
+);
+
+DROP TABLE IF EXISTS `TiposContacto`;
+CREATE TABLE TiposContacto(
+TipoContactoID INT NOT NULL AUTO_INCREMENT,
+TipoContactoNombre VARCHAR(255) UNIQUE,
+PRIMARY KEY(TipoContactoID)
+);
+
+DROP TABLE IF EXISTS `Localidades`;
+CREATE TABLE Localidades(
+LocalidadID INT NOT NULL AUTO_INCREMENT,
+LocalidadNombre VARCHAR(255) UNIQUE,
+PRIMARY KEY (LocalidadID)
 );
