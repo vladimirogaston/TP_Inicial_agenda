bf04a22cfd05de5d3e6f86b0c5bd823438d9b8f1
vladimirogaston <vladimirogaston@gmail.com>
2020-09-16 17:43:51 -0300

    actualizo la vista issue #19

--- src/main/java/presentacion/vista/Vista.java
+++ src/main/java/presentacion/vista/Vista.java
@@ -15,6 +15,11 @@ import dto.PersonaDTO;
 import persistencia.dao.mysql.Conexion;
 
 import javax.swing.JButton;
+import javax.swing.JToolBar;
+import java.awt.FlowLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Vista {
 	JFrame frame;
@@ -26,22 +31,24 @@ public class Vista {
 	JButton btnBorrar;
 	JButton btnReporte;
 	JButton btnEditar;
+	private JPanel panel_1;
 
 	public Vista() {
 		super();
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 910, 392);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setTitle("Agenda");
 
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 262);
+		panel.setBounds(0, 0, 904, 347);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 
 		JScrollPane spPersonas = new JScrollPane();
-		spPersonas.setBounds(10, 11, 414, 182);
+		spPersonas.setBounds(0, 39, 904, 270);
 		panel.add(spPersonas);
 
 		modelPersonas = new DefaultTableModel(null, nombreColumnas);
@@ -53,22 +60,41 @@ public class Vista {
 		tablaPersonas.getColumnModel().getColumn(1).setResizable(false);
 
 		spPersonas.setViewportView(tablaPersonas);
-
-		btnAgregar = new JButton("Agregar");
-		btnAgregar.setBounds(10, 228, 89, 23);
-		panel.add(btnAgregar);
-
-		btnEditar = new JButton("Editar");
-		btnEditar.setBounds(109, 228, 89, 23);
-		panel.add(btnEditar);
-
-		btnBorrar = new JButton("Borrar");
-		btnBorrar.setBounds(208, 228, 89, 23);
-		panel.add(btnBorrar);
-
-		btnReporte = new JButton("Reporte");
-		btnReporte.setBounds(307, 228, 89, 23);
-		panel.add(btnReporte);
+		
+		panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panel_1.setBounds(0, 308, 904, 39);
+		panel.add(panel_1);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		panel_1.add(toolBar);
+		
+				btnAgregar = new JButton("Agregar");
+				toolBar.add(btnAgregar);
+						
+								btnBorrar = new JButton("Borrar");
+								toolBar.add(btnBorrar);
+								
+										btnEditar = new JButton("Editar");
+										toolBar.add(btnEditar);
+						
+								btnReporte = new JButton("Reporte");
+								toolBar.add(btnReporte);
+								
+								JMenuBar menuBar = new JMenuBar();
+								menuBar.setBounds(0, 0, 904, 39);
+								panel.add(menuBar);
+								
+								JMenu mnNewMenu = new JMenu("Opciones");
+								menuBar.add(mnNewMenu);
+								
+								JMenuItem mntmNewMenuItem = new JMenuItem("Localidades");
+								mnNewMenu.add(mntmNewMenuItem);
+								
+								JMenuItem mntmNewMenuItem_1 = new JMenuItem("Tipos");
+								mnNewMenu.add(mntmNewMenuItem_1);
 	}
 
 	public void showMessages(String message) {
