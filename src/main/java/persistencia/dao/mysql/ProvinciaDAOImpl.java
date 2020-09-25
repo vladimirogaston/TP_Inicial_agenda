18a24ceb92e5eb472b8ba67e099af826d01325da
pabloabregu <pablo.abregu@hotmail.com>
2020-09-24 23:50:22 -0300

    add Editar Provincia

--- src/main/java/persistencia/dao/mysql/ProvinciaDAOImpl.java
+++ src/main/java/persistencia/dao/mysql/ProvinciaDAOImpl.java
@@ -14,24 +14,44 @@ import presentacion.controlador.DatabaseException;
 public class ProvinciaDAOImpl implements ProvinciaDAO {
 
 	static final String insert = "INSERT INTO Provincia(ProvinciaNombre, PaisID) VALUES (?, (SELECT PaisID FROM Pais P WHERE P.PaisNombre = ?))";
+	static final String update = "UPDATE Provincia SET ProvinciaNombre = ?,	PaisID = (SELECT PaisID FROM Pais P WHERE P.PaisNombre = ?) WHERE ProvinciaID = ?";
 	final String readall = "SELECT ProvinciaID, ProvinciaNombre, PaisNombre FROM Provincia PR LEFT JOIN Pais PA ON PR.PaisID = PA.PaisID";
 	static final String readbyid = "SELECT * FROM Provincia WHERE ProvinciaID = ?";
 	
 	@Override
-	public boolean update(ProvinciaDTO p) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(ProvinciaDTO provinciaDTO) {
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isInsertExitoso = false;
+		try {
+			statement = conexion.prepareStatement(update);
+			statement.setString(1, provinciaDTO.getNombre());
+			statement.setString(2, provinciaDTO.getPais());
+			statement.setInt(3, provinciaDTO.getId());
+			if (statement.executeUpdate() > 0) {
+				conexion.commit();
+				isInsertExitoso = true;
+			}
+		} catch (SQLException e) {
+			try {
+				conexion.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			throw new DatabaseException("La Provincia ya existe.");
+		}
+		return isInsertExitoso;
 	}
 
 	@Override
-	public boolean insert(ProvinciaDTO personaDTO) {
+	public boolean insert(ProvinciaDTO provinciaDTO) {
 		PreparedStatement statement;
 		Connection conexion = Conexion.getConexion().getSQLConexion();
 		boolean isInsertExitoso = false;
 		try {
 			statement = conexion.prepareStatement(insert);
-			statement.setString(1, personaDTO.getNombre());
-			statement.setString(2, personaDTO.getPais());
+			statement.setString(1, provinciaDTO.getNombre());
+			statement.setString(2, provinciaDTO.getPais());
 			if (statement.executeUpdate() > 0) {
 				conexion.commit();
 				isInsertExitoso = true;
