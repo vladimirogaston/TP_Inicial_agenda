fb9cd0050b85d77ca222d19f23651a6ef4c2b0dd
vladimirogaston <vladimirogaston@gmail.com>
2020-10-02 16:36:59 -0300

    fix provview

--- src/main/java/presentacion/views/ProvinciaView.java
+++ src/main/java/presentacion/views/ProvinciaView.java
@@ -8,20 +8,15 @@ import java.awt.event.WindowEvent;
 import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
-import dto.LocalidadDTO;
 import dto.ProvinciaDTO;
 
 @SuppressWarnings("serial")
